d8d786c0d33552468f4237ecf8f642ca2d613e8a
Yh0sun <hy770819@naver.com>
2022-04-26 11:48:45 +0900

    jar made

---
+++ src/main/java/com/basic/myspringboot/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.basic.myspringboot.repository;
+
+import com.basic.myspringboot.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
